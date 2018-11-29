4b82bda7f6747a1c7f18eb8c32e5a2269867e50a
Shine <1333278+varioso@users.noreply.github.com>
2018-11-29 14:49:53 +0200

    Update Topnav.js

--- src/client/components/Navigation/Topnav.js
+++ src/client/components/Navigation/Topnav.js
@@ -353,9 +353,9 @@ class Topnav extends React.Component {
           <div className={classNames('left', { 'Topnav__mobile-hidden': searchBarActive })}>
             <Link className="Topnav__brand" to="/">
               <i className="iconfont icon-busy Topnav__brand-icon" />
-              busy
+              ftin
             </Link>
-            <span className="Topnav__version">beta</span>
+            <span className="Topnav__version">alpha</span>
           </div>
           <div className={classNames('center', { mobileVisible: searchBarActive })}>
             <div className="Topnav__input-container">
@@ -380,7 +380,7 @@ class Topnav extends React.Component {
                     defaultMessage: 'What are you looking for?',
                   })}
                   autoCapitalize="off"
-                  autoCorrect="off"
+                  autoCorrect="off" 
                 />
               </AutoComplete>
               <i className="iconfont icon-search" />
