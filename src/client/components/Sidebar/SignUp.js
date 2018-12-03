7588f3533ab76a0b30de26127da99b097e5c8f59
Shine <1333278+varioso@users.noreply.github.com>
2018-12-03 21:34:12 +0200

    Update SignUp.js

--- src/client/components/Sidebar/SignUp.js
+++ src/client/components/Sidebar/SignUp.js
@@ -6,7 +6,6 @@ const SignUp = () => (
   <div className="SidebarBlock">
     <h3 className="SidebarBlock__title">
       <FormattedMessage id="" defaultMessage="" />
-      New to FTIN
     </h3>
     <a target="_blank" rel="noopener noreferrer" href={process.env.SIGNUP_URL}>
       <button className="SidebarBlock__button">
