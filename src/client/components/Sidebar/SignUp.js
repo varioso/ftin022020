217fb16121ae30b7ca9de54d61a567902fa82a5e
Shine <1333278+varioso@users.noreply.github.com>
2018-12-03 22:36:51 +0200

    Update SignUp.js

--- src/client/components/Sidebar/SignUp.js
+++ src/client/components/Sidebar/SignUp.js
@@ -5,7 +5,7 @@ import './SidebarBlock.less';
 const SignUp = () => (
   <div className="SidebarBlock">
     <h3 className="SidebarBlock__title">
-      <FormattedMessage id="new_to_busy" defaultMessage="New to FTIN?" />
+      <FormattedMessage id="new_to_ftin" defaultMessage="New to FTIN?" />
     </h3>
     <a target="_blank" rel="noopener noreferrer" href={process.env.SIGNUP_URL}>
       <button className="SidebarBlock__button">
