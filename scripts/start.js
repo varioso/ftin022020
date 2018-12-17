23cc1909e68a67c340b0f4b4c0b3b90a1f53714d
Shine <1333278+varioso@users.noreply.github.com>
2018-12-18 00:02:20 +0200

    Update start.js

--- scripts/start.js
+++ scripts/start.js
@@ -27,7 +27,7 @@ async function main() {
     noInfo: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin' 'X-Requested-With' 'Content-Type' 'Accept' 'Authorization',
+      'Access-Control-Allow-Headers': 'Origin' 'X-PINGOTHER' 'X-Requested-With' 'Content-Type' 'Accept' 'Authorization',
     },
     if (req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'),
