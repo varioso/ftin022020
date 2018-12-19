6276d47ff55f4edd3031be23df2342b00aeb635d
Shine <1333278+varioso@users.noreply.github.com>
2018-12-19 11:31:46 +0200

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -26,6 +26,19 @@ const IS_DEV = process.env.NODE_ENV === 'development';
 
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-PINGOTHER, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
+
 if (IS_DEV) {
   app.use(express.static(paths.publicRuntime(), { index: false }));
 } else {
