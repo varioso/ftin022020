8c72e5df0c8508f250ac2a48a158b9f90329c784
Shine <1333278+varioso@users.noreply.github.com>
2018-12-20 15:25:01 +0200

    Update steemConnectAPI.js

--- src/client/steemConnectAPI.js
+++ src/client/steemConnectAPI.js
@@ -1,9 +1,10 @@
 import sc2 from 'sc2-sdk';
 
-const api = sc2.Initialize({
-  app: process.env.STEEMCONNECT_CLIENT_ID,
-  baseURL: process.env.STEEMCONNECT_HOST,
-  callbackURL: process.env.STEEMCONNECT_REDIRECT_URL,
+sc2.Initialize({
+app: "FTI",
+baseURL: "https://steemconnect.com",
+callbackURL: "ftin.herokuapp.com",
+scope: ["GET, PUT, FETCH, POST"]
 });
 
 export default api;
