330e3a7e528958bda7c9d42bb1776c6fb10ba67b
Shine <1333278+varioso@users.noreply.github.com>
2018-12-22 14:25:09 +0200

    Update LoginModal.js

--- src/client/components/LoginModal.js
+++ src/client/components/LoginModal.js
@@ -55,7 +55,7 @@ class LoginModal extends React.Component {
         <div className="LoginModal__body">
           <i className="iconfont icon-busy LoginModal__icon" />
           <span className="LoginModal__login-title">
-            <FormattedMessage id="login_to_busy" defaultMessage="Login to FTIn" />
+            <FormattedMessage id="login_to_FTIN" defaultMessage="Login to FTIn" />
           </span>
           <span className="LoginModal__login-description">
             <FormattedMessage
