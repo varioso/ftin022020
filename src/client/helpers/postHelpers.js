54b98b6e82a065fcaafa405b25c0bc7836cfe06b
Shine <1333278+varioso@users.noreply.github.com>
2019-01-12 13:54:31 +0200

    Update postHelpers.js

--- src/client/helpers/postHelpers.js
+++ src/client/helpers/postHelpers.js
@@ -68,8 +68,8 @@ export function getContentImages(content, parsed = false) {
 
 export function createPostMetadata(body, tags, oldMetadata = {}) {
   let metaData = {
-    community: 'busy',
-    app: `busy/${appVersion}`,
+    community: 'ftin',
+    app: `ftin/${appVersion}`,
     format: 'markdown',
   };
 
