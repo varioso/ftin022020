374d27927b5969350da80bbc6d5363bacaf5e622
Shine <1333278+varioso@users.noreply.github.com>
2019-01-14 17:43:43 +0200

    Update TopicSelector.js

--- src/client/components/TopicSelector.js
+++ src/client/components/TopicSelector.js
@@ -14,11 +14,11 @@ const TopicSelector = ({ sort, isSingle, topics, onTopicClose, onSortChange }) =
         ))}
     </div>
     <SortSelector sort={sort} onChange={onSortChange}>
-      <SortSelector.Item key="active">
-        <FormattedMessage id="sort_active" defaultMessage="Active" />
-      </SortSelector.Item>
       <SortSelector.Item key="created">
         <FormattedMessage id="sort_created" defaultMessage="Created" />
+      </SortSelector.Item>
+      <SortSelector.Item key="active">
+        <FormattedMessage id="sort_active" defaultMessage="Active" />
       </SortSelector.Item>
       <SortSelector.Item key="trending">
         <FormattedMessage id="sort_trending" defaultMessage="Trending" />
