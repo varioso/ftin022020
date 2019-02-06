c8145244483d2630c5e27e1781cc41c161227c86
Shine <1333278+varioso@users.noreply.github.com>
2019-02-06 17:53:18 +0200

    Update PostModal.js

--- src/client/post/PostModal.js
+++ src/client/post/PostModal.js
@@ -94,7 +94,7 @@ class PostModal extends React.Component {
       shownPostContents,
     } = this.props;
     const { author, permlink, title, url } = currentShownPost;
-    const baseURL = window ? window.location.origin : 'https://ftin.herokuapp.com';
+    const baseURL = window ? window.location.origin : 'http://network.futuretechinsider.com';
     const postURL = `${baseURL}${dropCategory(url)}`;
     const twitterText = `"${encodeURIComponent(title)}" by @${author}`;
     const twitterShareURL = getTwitterShareURL(twitterText, postURL);
