bb02bbd4d038502ce33d738d97879e588f08756d
Shine <1333278+varioso@users.noreply.github.com>
2019-02-07 13:50:37 +0200

    Update ampRenderer.js

--- src/server/renderers/ampRenderer.js
+++ src/server/renderers/ampRenderer.js
@@ -63,7 +63,7 @@ function getContext(post, body, appUrl) {
     },
     publisher: {
       '@type': 'Organization',
-      name: 'FTIN',
+      name: 'FTI Network',
       logo: {
         '@type': 'ImageObject',
         url: `${appUrl}/images/logo-brand.png`,
