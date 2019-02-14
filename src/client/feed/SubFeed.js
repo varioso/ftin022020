391921b0a853a4e3007e36bba7e97afdfd90c041
Shine <1333278+varioso@users.noreply.github.com>
2019-02-14 14:35:35 +0200

    Update SubFeed.js

--- src/client/feed/SubFeed.js
+++ src/client/feed/SubFeed.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import Cookie from 'js-cookie';
 import _ from 'lodash';
-//import { showPostModal } from '../app/appActions';
+import { showPostModal } from '../app/appActions';
 import { getFeedContent, getMoreFeedContent } from './feedActions';
 
 import {
