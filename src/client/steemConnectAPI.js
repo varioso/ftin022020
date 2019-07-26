eece5a35a62eff7c6074c3b0290fce1b06a5b900
Shine <1333278+varioso@users.noreply.github.com>
2019-07-26 12:54:20 +0300

    Update steemConnectAPI.js

--- src/client/steemConnectAPI.js
+++ src/client/steemConnectAPI.js
@@ -2,7 +2,7 @@ import sc2 from 'sc2-sdk';
 
 const api = sc2.Initialize({
 app: process.env.STEEMCONNECT_CLIENT_ID,
-  baseURL: process.env.STEEMCONNECT_HOST,
+  baseURL: https://api.steemconnect.com/api/oauth2/authorize,
   callbackURL: process.env.STEEMCONNECT_REDIRECT_URL,
 });
 
