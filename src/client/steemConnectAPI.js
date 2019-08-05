0241b23d3c9404860b9b59d5e5b2efc023b9d89e
Shine <1333278+varioso@users.noreply.github.com>
2019-08-05 17:34:55 +0300

    Update steemConnectAPI.js

--- src/client/steemConnectAPI.js
+++ src/client/steemConnectAPI.js
@@ -2,7 +2,7 @@ import steemconnect from 'steemconnect';
 
 const api = new steemconnect.Client({
 app: "ftinetwork",
-  baseURL: "https://steemconnect.com",
+  //baseURL: "https://steemconnect.com",
   callbackURL: "http://network.futuretechinsider.com/callback",
 });
 
