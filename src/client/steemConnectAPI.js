cefc85fca5085f49ee2cdebee654f2c3feec87fb
Shine <1333278+varioso@users.noreply.github.com>
2019-08-07 13:54:37 +0300

    Update steemConnectAPI.js

--- src/client/steemConnectAPI.js
+++ src/client/steemConnectAPI.js
@@ -1,9 +1,81 @@
-import steemconnect from 'steemconnect';
+import sc2 from 'sc2-sdk';
 
-const api = new steemconnect.Client({
-app: process.env.STEEMCONNECT_CLIENT_ID,
-  //baseURL: "https://steemconnect.com",
-  callbackURL: process.env.STEEMCONNECT_REDIRECT_URL,
-});
+function sc2Extended() {
+  const sc2api = sc2.Initialize({
+    app: process.env.STEEMCONNECT_CLIENT_ID,
+    baseURL: process.env.STEEMCONNECT_HOST,
+    callbackURL: process.env.STEEMCONNECT_REDIRECT_URL,
+  });
+
+const copied = Object.assign(
+    Object.create(Object.getPrototypeOf(sc2api)),
+    sc2api,
+    {
+      followObject(follower, followingObject, cb) {
+        const params = {
+          required_auths: [],
+          required_posting_auths: [follower],
+          id: 'follow_wobject',
+          json: JSON.stringify([
+            'follow',
+            { user: follower, author_permlink: followingObject, what: ['feed'] },
+          ]),
+        };
+        return this.broadcast([['custom_json', params]], cb);
+      },
+    },
+    {
+      unfollowObject(unfollower, unfollowingObject, cb) {
+        const params = {
+          required_auths: [],
+          required_posting_auths: [unfollower],
+          id: 'follow_wobject',
+          json: JSON.stringify([
+            'follow',
+            { user: unfollower, author_permlink: unfollowingObject, what: [] },
+          ]),
+        };
+        return this.broadcast([['custom_json', params]], cb);
+      },
+    },
+    {
+      rankingObject(username, author, permlink, authorPermlink, rate, cb) {
+        const params = {
+          required_auths: [],
+          required_posting_auths: [username],
+          id: 'wobj_rating',
+          json: JSON.stringify({ author, permlink, author_permlink: authorPermlink, rate }),
+        };
+        return this.broadcast([['custom_json', params]], cb);
+      },
+    },
+    {
+      assignProposition(account, campaignId, object, cb) {
+        const params = {
+          required_auths: [],
+          required_posting_auths: [account],
+          id: 'waivio_assign_campaign',
+          json: JSON.stringify({ account, campaign_id: campaignId, object }),
+        };
+        return this.broadcast([['custom_json', params]], cb);
+      },
+    },
+    {
+      declineProposition(account, campaignId, object, cb) {
+        const params = {
+          required_auths: [],
+          required_posting_auths: [account],
+          id: 'waivio_decline_campaign',
+          json: JSON.stringify({ account, campaign_id: campaignId, object }),
+        };
+        return this.broadcast([['custom_json', params]], cb);
+      },
+    },
+  );
+
+  return copied;
+}
+
+const api = sc2Extended();
 
 export default api;
