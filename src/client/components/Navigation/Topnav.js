c6ae0c49eebd302478d6bbd52c7e1dda1ad02d9f
Shine <1333278+varioso@users.noreply.github.com>
2020-02-19 15:37:47 +0200

    Update Topnav.js

--- src/client/components/Navigation/Topnav.js
+++ src/client/components/Navigation/Topnav.js
@@ -23,7 +23,7 @@ import Popover from '../Popover';
 import Notifications from './Notifications/Notifications';
 import LanguageSettings from './LanguageSettings';
 import './Topnav.less';
-
+import { USER_METADATA_KEY } from '../../helpers/constants';
 @injectIntl
 @withRouter
 @connect(
@@ -150,7 +150,15 @@ class Topnav extends React.Component {
   menuForLoggedIn = () => {
     const { intl, username, notifications, userSCMetaData, loadingNotifications } = this.props;
     const { searchBarActive, notificationsPopoverVisible, popoverVisible } = this.state;
-    const lastSeenTimestamp = _.get(userSCMetaData, 'notifications_last_timestamp');
+    
+    let lastSeenTimestamp;
+    try {
+      lastSeenTimestamp = JSON.parse(localStorage.getItem(USER_METADATA_KEY))
+        .notifications_last_timestamp;
+    } catch (error) {
+      lastSeenTimestamp = _.get(userSCMetaData, 'notifications_last_timestamp');
+    }
+    
     const notificationsCount = _.isUndefined(lastSeenTimestamp)
       ? _.size(notifications)
       : _.size(
