638ddb4e93bb772f328491ce262c126fd15a2966
Shine <1333278+varioso@users.noreply.github.com>
2020-02-19 15:54:40 +0200

    Update metadata.js

--- src/client/helpers/metadata.js
+++ src/client/helpers/metadata.js
@@ -1,12 +1,59 @@
 import omit from 'lodash/omit';
 import SteemConnect from '../steemConnectAPI';
+import { USER_METADATA_KEY } from './constants';
 
-const getMetadata = () => SteemConnect.me().then(resp => resp.user_metadata);
+const initialSetting = {
+  locale: 'auto',
+  votingPower: 'on',
+  votePercent: 10000,
+  showNSFWPosts: false,
+  nightmode: false,
+  rewriteLinks: false,
+  loading: false,
+  exitPageSetting: true,
+  rewardSetting: 50,
+  useBeta: false,
+};
+
+// sc2 metadata support discontinuation preparation
+// const getMetadata = () => SteemConnect.me().then(resp => resp.user_metadata);
+// note that using Promise in some place is just to provide backward compatibility to old codes. refactoring is needed later.
+export const getMetadata = () => {
+  let localMetadata = JSON.parse(localStorage.getItem(USER_METADATA_KEY));
+  if (localMetadata) {
+    return Promise.resolve(localMetadata);
+  }
+
+  let promise; // due to lint error
+  SteemConnect.me().then(resp => {
+    try {
+      if (resp.user_metadata) {
+        localStorage.setItem(USER_METADATA_KEY, JSON.stringify(resp.user_metadata));
+        promise = resp.user_metadata;
+      } else {
+        localMetadata = {
+          settings: initialSetting,
+        };
+        localStorage.setItem(USER_METADATA_KEY, JSON.stringify(localMetadata));
+        promise = Promise.resolve(localMetadata);
+      }
+    } catch (err) {
+      promise = Promise.reject(new Error(err));
+    }
+  });
+
+  return promise;
+};
+
+export const updateUserMetadata = metadata => {
+  localStorage.setItem(USER_METADATA_KEY, JSON.stringify(metadata));
+  return Promise.resolve(metadata);
+};
 
 export const saveSettingsMetadata = settings =>
   getMetadata()
     .then(metadata =>
-      SteemConnect.updateUserMetadata({
+      updateUserMetadata({
         ...metadata,
         settings: {
           ...metadata.settings,
@@ -14,22 +61,12 @@ export const saveSettingsMetadata = settings =>
         },
       }),
     )
-    .then(resp => resp.user_metadata.settings);
-
-export const setLocaleMetadata = locale =>
-  getMetadata()
-    .then(metadata =>
-      SteemConnect.updateUserMetadata({
-        ...metadata,
-        locale,
-      }),
-    )
-    .then(resp => resp.user_metadata.locale);
+    .then(resp => resp.settings);
 
 export const addDraftMetadata = draft =>
   getMetadata()
     .then(metadata =>
-      SteemConnect.updateUserMetadata({
+      updateUserMetadata({
         ...metadata,
         drafts: {
           ...metadata.drafts,
@@ -37,22 +74,22 @@ export const addDraftMetadata = draft =>
         },
       }),
     )
-    .then(resp => resp.user_metadata.drafts[draft.id]);
+    .then(resp => resp.drafts[draft.id]);
 
 export const deleteDraftMetadata = draftIds =>
   getMetadata()
     .then(metadata =>
-      SteemConnect.updateUserMetadata({
+      updateUserMetadata({
         ...metadata,
         drafts: omit(metadata.drafts, draftIds),
       }),
     )
-    .then(resp => resp.user_metadata.drafts);
+    .then(resp => resp.drafts);
 
 export const toggleBookmarkMetadata = (id, author, permlink) =>
   getMetadata()
     .then(metadata =>
-      SteemConnect.updateUserMetadata({
+      updateUserMetadata({
         ...metadata,
         bookmarks:
           metadata.bookmarks && metadata.bookmarks[id]
@@ -60,16 +97,16 @@ export const toggleBookmarkMetadata = (id, author, permlink) =>
             : { ...metadata.bookmarks, [id]: { id, author, permlink } },
       }),
     )
-    .then(resp => resp.user_metadata.bookmarks);
+    .then(resp => resp.bookmarks);
 
 export const saveNotificationsLastTimestamp = lastTimestamp =>
   getMetadata()
     .then(metadata =>
-      SteemConnect.updateUserMetadata({
+      updateUserMetadata({
         ...metadata,
         notifications_last_timestamp: lastTimestamp,
       }),
     )
-    .then(resp => resp.user_metadata.notifications_last_timestamp);
+    .then(resp => resp.notifications_last_timestamp);
 
 export default getMetadata;
